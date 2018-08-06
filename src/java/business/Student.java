976055415569611c4a61dcea93c7847d4c0625fa
Nguyen Ngoc Hung <Nguyen Ngoc Hung@NguyenNgocHung>
2018-08-06 09:08:42 +0700

    V.2

--- src/java/business/Student.java
+++ src/java/business/Student.java
@@ -1,17 +1,34 @@
 
 package business;
 
+import java.time.LocalDate;
+
 
 public class Student {
     private int id;
     private String studentName;
+    private LocalDate birthday;
 
     public Student() {
+    }
+
+    public Student(int id, String studentName, LocalDate birthday) {
+        this.id = id;
+        this.studentName = studentName;
+        this.birthday = birthday;
     }
 
     public Student(int id, String studentName) {
         this.id = id;
         this.studentName = studentName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
     }
 
     public int getId() {
---
+++ src/java/controller/StudentController.java
@@ -0,0 +1,15 @@
+
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hung")
+public class StudentController extends HttpServlet{
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    
+    }
+}
